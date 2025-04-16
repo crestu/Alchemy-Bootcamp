7d61d59edb8215acd06336256f694adf3ddb46f6
crestu <143304087+crestu@users.noreply.github.com>
2025-04-16 15:08:09 -0500

    Create hashMessage.js
    Your first step is to take the string message passed in and turn it into an array of UTF-8 bytes. You can do so with the utf8ToBytes function.
    Then take the keccak256 hash of those bytes and return this value.

---
+++ Hash Message/hashMessage.js
@@ -0,0 +1,17 @@
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex, utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    const bytes = utf8ToBytes(message);
+
+    const hash = keccak256(bytes);
+
+    return hash
+
+}
+
+module.exports = hashMessage;
+
+  // if ((toHex(hash))===(toHex(message))){
+    //     return message
+    // }
